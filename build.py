7aa158a1e370144ed65632ef6055072441083c7d
Glen Wasserfall <Glen.Wasserfall@smollan.com>
2022-03-30 14:44:31 +0200

    Update

---
+++ build.py
@@ -0,0 +1,22 @@
+import sys
+import base64
+import json
+import os
+
+with open("./config.json", "r") as f:
+    config = json.loads(f.read())
+
+with open(config["template"], "r") as f:
+    template = json.loads(f.read())
+
+for card in config["cards"]:
+
+    with open(card["image"], "rb") as f:
+        content = f.read()
+        image_url = f"data:image/jpeg;base64,{base64.b64encode(content)}"
+
+    template["body"][0]["items"][0]["columns"][0]["items"][0]["url"] = image_url
+
+    with open(card["output"], "w") as f:
+        f.write(json.dumps(template, indent=2))
+
--- icons/README.md
+++ icons/README.md
@@ -1,5 +1,3 @@
 Image size is 300x300
-
 SVG scaled to 260x260
-
 5px border of same color around image
